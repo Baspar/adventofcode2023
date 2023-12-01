83d12491457da0754535e132f8e1a090d0f36dbc
Baspar <me@bastien-laine.io>
2023-12-01 22:00:01 +0100

    Day01

--- go/day01/main.go
+++ go/day01/main.go
@@ -1,20 +1,80 @@
 package main
 
 import (
+	"fmt"
+	"regexp"
+
 	utils "github.com/baspar/adventofcode2023/internal"
 )
 
 type DayImpl struct {
+	Lines []string
 }
 
 func (d *DayImpl) Init(lines []string) error {
+	d.Lines = lines
 	return nil
 }
+func valueOf(val []byte) int {
+	switch string(val) {
+	case "one":
+		return 1
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	}
+
+	return int(val[0] - '0')
+}
+
+func (d *DayImpl) sumWith(r *regexp.Regexp) int {
+	sum := 0
+	for _, l := range d.Lines {
+		var (
+			line = []byte(l)
+			startSearchAt = 0
+			first, last []byte
+		)
+		for {
+			matchLocation := r.FindIndex(line[startSearchAt:])
+			if matchLocation == nil {
+				break
+			}
+
+			from, to := matchLocation[0]+startSearchAt, matchLocation[1]+startSearchAt
+			last = line[from:to]
+			if first == nil {
+				first = last
+			}
+
+			startSearchAt = from + 1
+		}
+		value := 10*valueOf(first) + valueOf(last)
+		sum += value
+	}
+	return sum
+}
+
 func (d *DayImpl) Part1() (string, error) {
-	return "", nil
+	sum := d.sumWith(regexp.MustCompile(`\d`))
+	return fmt.Sprint(sum), nil
 }
 func (d *DayImpl) Part2() (string, error) {
-	return "", nil
+	sum := d.sumWith(regexp.MustCompile(`\d|one|two|three|four|five|six|seven|eight|nine`))
+	return fmt.Sprint(sum), nil
 }
 
 func main() {
--- go/day01/main_test.go
+++ go/day01/main_test.go
@@ -7,25 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = ``
+// var input = `1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet`
+var input = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
 
 var inputs = map[string][2]string{
-	input: {"", ""},
+	input: {"142", ""},
 }
 
-func TestPart1(t *testing.T) {
-	assert := assert.New(t)
-
-	for input, expectedRes := range inputs {
-		d := &DayImpl{}
-		d.Init(utils.SanitizeInput(input))
-
-		res, err := d.Part1()
-
-		assert.Equal(expectedRes[0], res)
-		assert.Nil(err)
-	}
-}
+// func TestPart1(t *testing.T) {
+// 	assert := assert.New(t)
+//
+// 	for input, expectedRes := range inputs {
+// 		d := &DayImpl{}
+// 		d.Init(utils.SanitizeInput(input))
+//
+// 		res, err := d.Part1()
+//
+// 		assert.Equal(expectedRes[0], res)
+// 		assert.Nil(err)
+// 	}
+// }
 
 func TestPart2(t *testing.T) {
 	assert := assert.New(t)
